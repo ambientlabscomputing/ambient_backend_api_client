b088785b0b68aea0aa68238a5bdcabaac31bb3e7
Jose Catarino <jfcatarino1@gmail.com>
2024-08-05 19:55:59 -0600

    run command event label (#22)

--- ambient_backend_api_client/models/post_clusters_response.py
+++ ambient_backend_api_client/models/post_clusters_response.py
@@ -29,7 +29,7 @@ class PostClustersResponse(BaseModel):
     PostClustersResponse
     """ # noqa: E501
     request_id: StrictInt
-    requested_ts: Optional[StrictStr] = '2024-08-04T18:21:36.154590'
+    requested_ts: Optional[StrictStr] = '2024-08-05T19:53:47.274973'
     location_root: Optional[StrictStr] = 'http://localhost:8001/requests/'
     refresh_interval: Optional[StrictInt] = 10
     location: Optional[StrictStr] = None
@@ -96,7 +96,7 @@ class PostClustersResponse(BaseModel):
 
         _obj = cls.model_validate({
             "request_id": obj.get("request_id"),
-            "requested_ts": obj.get("requested_ts") if obj.get("requested_ts") is not None else '2024-08-04T18:21:36.154590',
+            "requested_ts": obj.get("requested_ts") if obj.get("requested_ts") is not None else '2024-08-05T19:53:47.274973',
             "location_root": obj.get("location_root") if obj.get("location_root") is not None else 'http://localhost:8001/requests/',
             "refresh_interval": obj.get("refresh_interval") if obj.get("refresh_interval") is not None else 10,
             "location": obj.get("location"),
